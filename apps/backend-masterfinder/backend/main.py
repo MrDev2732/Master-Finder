316703039e382aef63c2dcb91e2b60ff3e702d43
MrDev2732 <vin.orellana@duocuc.cl>
2024-06-05 01:07:03 -0400

    feat: Se agregan funciones para login

--- apps/backend-masterfinder/backend/main.py
+++ apps/backend-masterfinder/backend/main.py
@@ -1,13 +1,22 @@
 import logging
+from typing import Annotated
+from datetime import datetime, timedelta
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Form, HTTPException, Cookie, Depends
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.middleware.cors import CORSMiddleware
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy import create_engine
+import jwt  # Reemplaza jose por pyjwt
 
 from backend.database.models import Base, Worker
 from backend.database.create_db import main as populate_db
 
+jinja2_template = Jinja2Templates(directory="templates")
+
+SECRET_KEY = 'Chupalo Yul CULIAO'
+TOKEN_SCOND_EXP = 60
 
 logging.basicConfig(level=logging.INFO,
                     format='%(levelname)s:     %(name)s - %(message)s')
@@ -31,28 +40,95 @@ app.add_middleware(
 )
 
 engine = create_engine("sqlite:///db.sqlite3")
-Session = sessionmaker(bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 @app.on_event("startup")
 def on_startup():
-    with Session() as session:
+    with SessionLocal() as session:
         try:
             session.query(Worker).first()
             logger.info("La base de datos ya está inicializada.")
         except Exception as e:
-            Base.metadata.create_all(engine)
+            Base.metadata.create_all(bind=engine)
             logger.info("Inicializando la base de datos...")
             populate_db(session)
-
 
 @app.get("/test")
 async def test():
     return {"detail": "Hello World"}
 
 
+def get_user(first_name: str, db: Session):
+    return db.query(Worker).filter(Worker.first_name == first_name).first()
+
+
+def authenticate_user(password: str, password_plane: str):
+    password_clean = password.split('#')[0]
+    return password_plane == password_clean
+
+
+def create_token(data: dict):
+    data_token = data.copy()
+    data_token["exp"] = datetime.utcnow() + timedelta(seconds=TOKEN_SCOND_EXP)
+    token_jwt = jwt.encode(data_token, key=SECRET_KEY, algorithm="HS256")
+    return token_jwt
+
+
+@app.get("/", response_class=HTMLResponse)
+def root(request: Request):
+    return jinja2_template.TemplateResponse("index.html", {"request": request})
+
+
+@app.get("/workers/dashboard", response_class=HTMLResponse)
+def dashboard(request: Request, db: Session = Depends(get_db), access_token: Annotated[str | None, Cookie()] = None):
+    if access_token is None:
+        return RedirectResponse("/", status_code=302)
+    try:
+        data_user = jwt.decode(access_token, key=SECRET_KEY, algorithms=["HS256"])
+        if get_user(data_user["first_name"], db) is None:
+            return RedirectResponse("/", status_code=302)
+        return jinja2_template.TemplateResponse("dashboard.html", {"request": request})
+    except jwt.ExpiredSignatureError:
+        return RedirectResponse("/", status_code=302)
+    except jwt.InvalidTokenError:
+        return RedirectResponse("/", status_code=302)
+
+
+@app.post("/workers/login")
+def login(first_name: Annotated[str, Form()], password: Annotated[str, Form()], db: Session = Depends(get_db)):
+    user_data = get_user(first_name, db)
+    if user_data is None or not authenticate_user(user_data.password_hash, password):
+        raise HTTPException(
+            status_code=401,
+            detail="Username or password no authorization"
+        )
+    token = create_token({"first_name": user_data.first_name})
+    return RedirectResponse(
+        "/workers/dashboard",
+        status_code=302,
+        headers={"set-cookie": f"access_token={token}; Max-Age={TOKEN_SCOND_EXP}"}
+    )
+
+
+@app.post("/workers/logout")
+def logout():
+    return RedirectResponse(
+        "/",
+        status_code=302,
+        headers={"set-cookie": "access_token=; Max-Age=0"}
+    )
+
+
 @app.get("/workers")
-async def get_workers():
-    with Session() as session:
-        workers = session.query(Worker).all()
-        return workers
+async def get_workers(db: Session = Depends(get_db)):
+    workers = db.query(Worker).all()
+    return workers
