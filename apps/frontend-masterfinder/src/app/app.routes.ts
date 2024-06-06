6a0db25fac66d074e54c8365f7861b2ea527718d
MrDev2732 <vin.orellana@duocuc.cl>
2024-06-06 14:29:46 -0400

    feat: Se agrega enrutamiento de la página home

--- apps/frontend-masterfinder/src/app/app.routes.ts
+++ apps/frontend-masterfinder/src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Route } from '@angular/router';
+import { Route, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { NgModule } from '@angular/core';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    { path: '', component: HomeComponent },
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(appRoutes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
