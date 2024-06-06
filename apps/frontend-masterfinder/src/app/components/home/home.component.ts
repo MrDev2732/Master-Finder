ea21fc92a1b8ffec35636b9a8ae88a563b693df4
AlexHernan7 <alexmazuela.v@gmail.com>
2024-06-06 00:50:38 -0400

    feature: funcion para probar el funcionamiento del login

--- apps/frontend-masterfinder/src/app/components/home/home.component.ts
+++ apps/frontend-masterfinder/src/app/components/home/home.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Worker } from '../../interfaces/worker';
+import { LoginService } from '../../services/login.service';
+import { Router } from '@angular/router';
+import { error } from 'console';
 
 @Component({
   selector: 'app-home',
@@ -8,4 +12,20 @@ import { CommonModule } from '@angular/common';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  constructor( private loginService: LoginService, private router: Router ){}
+
+
+
+  loginUser(username: string, password: string):void{
+    this.loginService.getUser(username, password).subscribe(
+      (response: any) => {
+        const user = response.first_name
+        const password = response.password
+      },
+      (error : any) => {
+        console.error('Error en el login del usuario', error);
+      }
+    )
+  }
+}
