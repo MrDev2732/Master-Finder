cfcc7fe044f0f44d96ffe1bec9f116c77e8df798
Vincenzo Orellana <vorellana@queplan.cl>
2024-06-07 23:46:53 -0400

    Se crea nuevo endpoint para obtener un Trabajador especifico

--- apps/backend-masterfinder/backend/services/endpoints/workers.py
+++ apps/backend-masterfinder/backend/services/endpoints/workers.py
@@ -1,9 +1,10 @@
 
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
-from backend.handlers.queries.user import get_user
+from backend.handlers.queries.worker import get_worker, get_all_workers
+from backend.database.session import get_db
 
 
 router = APIRouter()
@@ -11,7 +12,21 @@ router = APIRouter()
 
 @router.get("/worker")
 async def get_worker(db: Session = Depends(get_db)):
-    workers = get_user()
+    workers = get_worker()
+    # Asegúrate de que todos los campos sean serializables
+    workers_serializable = []
+    for worker in workers:
+        worker_dict = worker.__dict__.copy()
+        for key, value in worker_dict.items():
+            if isinstance(value, bytes):
+                worker_dict[key] = value.decode('utf-8', errors='replace')  # Decodifica bytes a string
+        workers_serializable.append(worker_dict)
+    return workers_serializable
+
+
+@router.get("/all-workers")
+async def get_workers(db: Session = Depends(get_db)):
+    workers = get_all_workers()
     # Asegúrate de que todos los campos sean serializables
     workers_serializable = []
     for worker in workers:
