e5a71586ff0d5a36dd378d62901be11e0f3c7837
Vincenzo Orellana <vorellana@queplan.cl>
2024-06-07 23:24:32 -0400

    refactor: Ahora se guarda el email en el JWT

--- apps/backend-masterfinder/backend/handlers/queries/user.py
+++ apps/backend-masterfinder/backend/handlers/queries/user.py
@@ -3,5 +3,5 @@ from sqlalchemy.orm import Session
 from backend.database.models import Worker
 
 
-def get_user(first_name: str, db: Session):
-    return db.query(Worker).filter(Worker.first_name == first_name).first()
+def get_user(email: str, db: Session):
+    return db.query(Worker).filter(Worker.email == email).first()
