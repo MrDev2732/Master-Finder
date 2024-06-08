f9d7bcac9ab16d3a499fbe6a73bd3911008e6694
AlexHernan7 <alexmazuela.v@gmail.com>
2024-06-08 18:20:09 -0400

    feature: se cambia menu de al appcomponent y se quita del home

--- apps/frontend-masterfinder/src/app/app.component.ts
+++ apps/frontend-masterfinder/src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router';
+import { NavbarComponent } from './components/layout/navbar/navbar.component';
+
+
+interface SidenavToggle{
+  screemWidth: number;
+  collapsed: boolean;
+}
 
 @Component({
   standalone: true,
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, RouterModule, NavbarComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'frontend-masterfinder';
+
+
+  isSidenavCollapsed = false;
+  screenWidth = 0;
+  onToggleSidenav(data: SidenavToggle): void{
+    this.screenWidth = data.screemWidth;
+    this.isSidenavCollapsed = data.collapsed;
+  }
 }
