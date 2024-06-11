24e289cc12cea1093c7ac56584633ae933f52f01
Draven <nicolasdraven91@gmail.com>
2024-06-11 16:05:45 -0400

    feature: Se agrega el componente de Soporte al path y se enruta.

--- apps/frontend-masterfinder/src/app/app.routes.ts
+++ apps/frontend-masterfinder/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Route, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginWorkerComponent } from './components/login-worker/login-worker.component';
 import { PerfilWorkerComponent } from './components/perfil-worker/perfil-worker.component';
+import { SoporteComponent } from './components/soporte/soporte.component';
 import { NgModule } from '@angular/core';
 
 
@@ -9,6 +10,7 @@ export const appRoutes: Route[] = [
     { path: '', component: HomeComponent },
     { path: 'login-worker', component: LoginWorkerComponent },
     { path: 'perfil-worker', component: PerfilWorkerComponent },
+    { path: 'soporte', component: SoporteComponent },
 ];
 
 @NgModule({
