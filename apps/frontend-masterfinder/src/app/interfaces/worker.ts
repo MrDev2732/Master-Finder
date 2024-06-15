99cd3cfc2cf2b26873ea051d0b932771ceb23097
AlexHernan7 <alexmazuela.v@gmail.com>
2024-06-14 23:06:38 -0400

    feat: se agrega interface para el registro

--- apps/frontend-masterfinder/src/app/interfaces/worker.ts
+++ apps/frontend-masterfinder/src/app/interfaces/worker.ts
@@ -1,14 +1,10 @@
 export interface Worker {
-    id: string;
-    enabled: boolean;
-    created_date: string;
-    modified_date: string;
     first_name: string;
     last_name: string;
     rut: string;
     contact_number: string;
     email: string;
-    subscription: boolean;
-    profile_description?: string;
     password: string;
+    specialty: string;
+    location: string;
 }
