3141ec9d5d4d03695e2fed869f0d7a3aa17eea71
Alexander Pizarro <alex.pizarroa@duocuc.cl>
2024-06-15 21:55:57 -0400

    feat:funcion listar

--- apps/frontend-masterfinder/src/app/components/home/home.component.ts
+++ apps/frontend-masterfinder/src/app/components/home/home.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { NavbarComponent } from '../layout/navbar/navbar.component';
+import { PublicacionService } from 'apps/frontend-masterfinder/src/services/publicacion.service';
+import { FormsModule } from '@angular/forms';
 
 
 interface SidenavToggle{
@@ -14,18 +16,44 @@ interface SidenavToggle{
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, NavbarComponent], // Asegúrate de importar FormsModule aquí
+  imports: [CommonModule, NavbarComponent, FormsModule], // Asegúrate de importar FormsModule aquí
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
   
 
-  constructor(private router: Router) {}
+  postings: any[] = [];
+  errorMessage: string = 'errooor!';
+
+  constructor(private router: Router,
+              private publicacionService: PublicacionService) {}
 
   login() {
     this.router.navigate(['/login-worker']);
   }
+
+  ngOnInit(): void {
+    this.loadPostings();
+  }
+
+  loadPostings(): void {
+    this.publicacionService.getAllPostings().subscribe({
+      next: (data) => {
+        this.postings = this.getRandomPostings(data, 3);
+      },
+      error: (error) => {
+        console.error('Error fetching postings:', error);
+      }
+    });
+  }
+
+
+    // Función para obtener postings al azar
+    getRandomPostings(data: any[], count: number): any[] {
+      const shuffled = data.sort(() => 0.5 - Math.random());
+      return shuffled.slice(0, count);
+    }
 
   
   isSidenavCollapsed = false;
