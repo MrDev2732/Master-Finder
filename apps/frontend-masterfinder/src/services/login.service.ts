9b324fb0c9f9ac0aa04ea986cd5675d2525fed89
AlexHernan7 <alexmazuela.v@gmail.com>
2024-06-19 13:08:22 -0400

    fix: se modifica servicio para que este maneje la respuesta del servidor

--- apps/frontend-masterfinder/src/services/login.service.ts
+++ apps/frontend-masterfinder/src/services/login.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
-  private apiUrl = 'http://localhost:8000/api/login'; // URL base de tu API FastAPI
+  private apiUrl = 'http://localhost:8000/api/login';
+  private logOut = 'http://localhost:8000/api/login/logout';
 
   constructor(private http: HttpClient) {}
 
@@ -28,6 +29,15 @@ export class LoginService {
   }
 
   logout(): Observable<any> {
-    return this.http.post(`${this.apiUrl}/logout`, {}, { withCredentials: true });
+    return this.http.post<any>(this.logOut, {}, { withCredentials: true, responseType: 'text' as 'json' }).pipe(
+      map(response => {
+        console.log('Logout response:', response);
+        return response;
+      }),
+      catchError((error: HttpErrorResponse) => {
+        console.error('Logout request failed', error);
+        return throwError(error);
+      })
+    );
   }
 }
