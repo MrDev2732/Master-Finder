2e6fa8fece2a43616d2540bdb7ad94883a6b0312
Draven <nicolasdraven91@gmail.com>
2024-06-20 13:29:06 -0400

    feature: Se agregan campos para hacer el filtro eficiente

--- apps/frontend-masterfinder/src/app/components/filtros/filtros.component.ts
+++ apps/frontend-masterfinder/src/app/components/filtros/filtros.component.ts
@@ -21,10 +21,40 @@ export class FiltrosComponent {
     this.loadPostings();
   }
 
-  loadPostings(): void {
+  // loadPostings(): void {
+  //   this.postingService.getAllPostings().subscribe({
+  //     next: (data) => {
+  //       this.postings = data;
+  //     },
+  //     error: (error) => {
+  //       console.error('Error fetching postings:', error);
+  //     }
+  //   });
+  // }
+
+  // onFilterSubmit(): void {
+  //   // Aquí puedes agregar lógica para enviar filtros específicos al backend
+  //   this.loadPostings();
+  // }
+
+  public selectedJobType: string = '';
+
+  public jobTypes: string[] = ['Carpintería', 'Electricidad', 'Gasfitería'];
+
+  onFilterSubmit(): void {
+    this.loadPostings(this.selectedJobType);
+  }
+  
+  loadPostings(jobType?: string): void {
     this.postingService.getAllPostings().subscribe({
       next: (data) => {
-        this.postings = data;
+        console.log(data);  // Esto te ayudará a confirmar la estructura de los datos
+        if (jobType) {
+          // Asegúrate de que 'job_type' es el nombre correcto del campo en los datos recibidos
+          this.postings = data.filter(posting => posting.job_type === jobType);
+        } else {
+          this.postings = data;
+        }
       },
       error: (error) => {
         console.error('Error fetching postings:', error);
@@ -32,8 +62,4 @@ export class FiltrosComponent {
     });
   }
 
-  onFilterSubmit(): void {
-    // Aquí puedes agregar lógica para enviar filtros específicos al backend
-    this.loadPostings();
-  }
 }
