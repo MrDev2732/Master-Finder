d745d618b766fe4de6cfc5f558d7e4beeb3a6729
Alexander Pizarro <alex.pizarroa@duocuc.cl>
2024-07-05 17:25:07 -0400

    feat: metodos para obtener ratings por id y obtener ratings de trabajadores del backend

--- apps/frontend-masterfinder/src/services/rating.service.ts
+++ apps/frontend-masterfinder/src/services/rating.service.ts
@@ -9,6 +9,8 @@ import { HttpParams } from '@angular/common/http';
 })
 export class RatingService {
   private baseUrl = 'http://localhost:8000/api/clients/create-rating'; // URL ajustada
+  private getRatingsUrl = 'http://localhost:8000/api/clients/get-ratings'; 
+  private getWorkerRatingsUrl = 'http://localhost:8000/api/workers/worker';
 
   constructor(private http: HttpClient, private authService: AuthService) { }
 
@@ -22,5 +24,18 @@ export class RatingService {
 
     return this.http.post(`${this.baseUrl}`, null, { headers, params });
 }
+
+  // Nuevo método para obtener ratings por worker_id
+  getRatingsByWorkerId(workerId: string): Observable<any> {
+    const token = this.authService.getToken(); // Obtener el token de autenticación
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    const params = new HttpParams().set('worker_id', workerId);
+
+    return this.http.get(`${this.getRatingsUrl}/${workerId}`, { headers, params });
+  }
+
+  getWorkerRatings(workerId: string): Observable<any[]> {
+    return this.http.get<any[]>(`${this.getWorkerRatingsUrl}/${workerId}/ratings`);
+  }
 
 }
